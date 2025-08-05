587d6211443933935e54c49596f3d0538a22054d
chingis <bodaral17r85@gmail.com>
2025-08-05 14:46:38 +0700

    update field  messages, now event stores an object and not a link

--- app/models/events.py
+++ app/models/events.py
@@ -2,14 +2,26 @@
 from pydantic import BaseModel, Field
 from datetime import datetime, timezone
 from typing import Optional, List
+from app.models.messages import Message
+
+class HostData(BaseModel):
+    ip: Optional[str] = None
+    hostname: Optional[str] = None
+    role: Optional[str] = None
+    model: Optional[str] = None
+    location: Optional[str] = None
+    services: List[str] = Field(default_factory=list)
 
 # Модель для Event
-class Event(BaseModel):
+class Event(HostData):
     id: Optional[str] = Field(None, alias="_id")
-    name: str
-    message_ids: List[str] = Field(default_factory=list)
-    created_at: datetime = Field(default_factory=datetime.now(timezone.utc))
-    updated_at: datetime = Field(default_factory=datetime.now(timezone.utc))
+    name: Optional[str] = None
+
+    messages: List[Message] = Field(default_factory=list)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    status: bool = True
 
     class Config:
         allow_population_by_field_name = True
@@ -19,3 +31,4 @@ class Event(BaseModel):
                 "message_ids": ["607f1f77bcf86cd799439012"]
             }
         }
+
